e53416e1ac135602aeba1a0ed4f2caae448ef531
NoLimitjke <kilin.daniil.99@gmail.com>
2026-02-04 09:27:45 +0500

    env changes

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,4 +1,10 @@
 import { treaty } from '@elysiajs/eden';
 import type { App } from '@/app/api/[...slugs]/route';
 
-export const client = treaty<App>('localhost:3000').api;
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
+if (!API_URL) {
+  throw new Error('NEXT_PUBLIC_API_URL не задан в .env');
+}
+
+export const client = treaty<App>(API_URL).api;
